33638e27136fd4bc65a75965402a12d3a31e3be1
Anton Zvontsov <azvontsov@gmail.com>
2022-01-25 15:22:27 -0500

    added svg to comments

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaHeart } from "react-icons/fa";
+import { FaHeart, FaComments } from "react-icons/fa";
 
 const PostItem = (props) => {
   const { post, updatePost, userEmail } = props;
@@ -41,15 +41,8 @@ const PostItem = (props) => {
           )}
 
           <h5>{likes.length} reactions</h5>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            role="img"
-            class="crayons-icon"
-          >
-            <path d="M10.5 5h3a6 6 0 110 12v2.625c-3.75-1.5-9-3.75-9-8.625a6 6 0 016-6zM12 15.5h1.5a4.501 4.501 0 001.722-8.657A4.5 4.5 0 0013.5 6.5h-3A4.5 4.5 0 006 11c0 2.707 1.846 4.475 6 6.36V15.5z"></path>
-          </svg>
+          <FaComments />
+
           <h5>5 comments</h5>
         </div>
         <div className="post_btns">
