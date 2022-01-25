44a30b4ebbe6de50397b8f80deaaded62216372e
Anton Zvontsov <azvontsov@gmail.com>
2022-01-25 15:14:25 -0500

    install react-icons & added likes svg to post

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaHeart } from "react-icons/fa";
 
 const PostItem = (props) => {
   const { post, updatePost, userEmail } = props;
@@ -16,7 +17,7 @@ const PostItem = (props) => {
       <div className="post_footer">
         <div className="reactions">
           {likes.includes(userEmail) ? (
-            <button
+            <a
               onClick={() =>
                 updatePost({
                   ...post,
@@ -24,10 +25,10 @@ const PostItem = (props) => {
                 })
               }
             >
-              liked
-            </button>
+              <FaHeart style={{ color: "red" }} />
+            </a>
           ) : (
-            <button
+            <a
               onClick={() =>
                 updatePost({
                   ...post,
@@ -35,18 +36,10 @@ const PostItem = (props) => {
                 })
               }
             >
-              not liked
-            </button>
+              <FaHeart style={{ color: "black" }} />
+            </a>
           )}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            role="img"
-            class="crayons-icon"
-          >
-            <path d="M18.884 12.595l.01.011L12 19.5l-6.894-6.894.01-.01A4.875 4.875 0 0112 5.73a4.875 4.875 0 016.884 6.865zM6.431 7.037a3.375 3.375 0 000 4.773L12 17.38l5.569-5.569a3.375 3.375 0 10-4.773-4.773L9.613 10.22l-1.06-1.062 2.371-2.372a3.375 3.375 0 00-4.492.25v.001z"></path>
-          </svg>
+
           <h5>{likes.length} reactions</h5>
           <svg
             xmlns="http://www.w3.org/2000/svg"
