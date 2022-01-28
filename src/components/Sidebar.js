dec8c37a725714af6f58c87f2b8bf546c0257b59
Anton Zvontsov <azvontsov@gmail.com>
2022-01-27 21:33:41 -0500

    added #tags to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,6 +9,24 @@ const Sidebar = ({ filter, setFilter }) => {
       <button onClick={(e) => setFilter({ ...filter, query: "#react" })}>
         #react
       </button>
+      <button onClick={(e) => setFilter({ ...filter, query: "#python" })}>
+        #python
+      </button>
+      <button onClick={(e) => setFilter({ ...filter, query: "#productivity" })}>
+        #productivity
+      </button>
+      <button onClick={(e) => setFilter({ ...filter, query: "#msw" })}>
+        #msw
+      </button>
+      <button onClick={(e) => setFilter({ ...filter, query: "#javascript" })}>
+        #javascript
+      </button>
+      <button onClick={(e) => setFilter({ ...filter, query: "#webdev" })}>
+        #webdev
+      </button>
+      <button onClick={(e) => setFilter({ ...filter, query: "#programming" })}>
+        #programming
+      </button>
     </div>
   );
 };
